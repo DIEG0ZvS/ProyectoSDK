7f923bf8bf1b2ea698dacf63e0d927480be66043
Jorge Luis Verastegui Ramirez <jlverastegui771@gmail.com>
2026-07-01 19:29:27 -0500

    Guardando avances

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,24 +4,12 @@ plugins {
 
 android {
     namespace = "edu.pe.udh.proyectosdk"
-<<<<<<< HEAD
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
-=======
     compileSdk = 34
->>>>>>> 417446008e2ecbe4a44530cf7a397590a8a95901
 
     defaultConfig {
         applicationId = "edu.pe.udh.proyectosdk"
         minSdk = 29
-<<<<<<< HEAD
-        targetSdk = 36
-=======
         targetSdk = 34
->>>>>>> 417446008e2ecbe4a44530cf7a397590a8a95901
         versionCode = 1
         versionName = "1.0"
 
@@ -30,16 +18,6 @@ android {
 
     buildTypes {
         release {
-<<<<<<< HEAD
-            optimization {
-                enable = false
-            }
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-=======
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -48,14 +26,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
->>>>>>> 417446008e2ecbe4a44530cf7a397590a8a95901
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
 dependencies {
-<<<<<<< HEAD
     implementation(libs.activity.ktx)
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
@@ -63,14 +39,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
-=======
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
->>>>>>> 417446008e2ecbe4a44530cf7a397590a8a95901
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,14 +11,12 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-<<<<<<< HEAD
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
-=======
->>>>>>> 417446008e2ecbe4a44530cf7a397590a8a95901
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
@@ -27,7 +25,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "ProyectoSDK"
 include(":app")
-<<<<<<< HEAD
-=======
- 
->>>>>>> 417446008e2ecbe4a44530cf7a397590a8a95901
